93a837d0cbb3ca3e5a0e42708f41f61161249008
luketheduke112 <lukedavidfisher03@gmail.com>
2022-05-29 00:23:36 -0400

    Modified init to fix pip packaging

--- CustomEnv/custom_env/__init__.py
+++ CustomEnv/custom_env/__init__.py
@@ -2,6 +2,6 @@ from gym.envs.registration import register
 
 register(
     id='CustomEnv-v0',
-    entry_point='CustomEnv.custom_env:CustomEnv',
+    entry_point='CustomEnv.custom_env.custom_env:CustomEnv',
     max_episode_steps=11
 )
